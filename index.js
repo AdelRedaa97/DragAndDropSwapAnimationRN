4c8fb2c08971fb4679ba31e4a198974e6416571c
Adel Reda <adelredaa97@gmail.com>
2023-03-07 04:11:39 +0200

    upgrade succeded android but failed ios

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import React from 'react'; // Added for firebase configuration
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import 'react-native-gesture-handler';
 
 function HeadlessCheck({ isHeadless }) {
     if (isHeadless) {
